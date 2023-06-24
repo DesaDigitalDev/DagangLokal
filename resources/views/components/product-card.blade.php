5624aa2f57ab0fa7e176e96ad83b0cc94f8b9acd
Dena Akbar Kholis <dena.ak@ad-ins.com>
2023-06-24 15:51:49 +0700

    penambahan component produk-card

---
+++ resources/views/components/product-card.blade.php
@@ -0,0 +1,43 @@
+<div class="col-3 col-sm-12 col-md-4 col-lg-3">
+    <div div class="card">
+        <img src="assets/img/dish1.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">TIRAMISU CAKE</h5>
+            <div class="stars mb-3">
+                <i class="fa fa-solid fa-star"></i>      
+                <i class="fa fa-solid fa-star"></i>    
+                <i class="fa fa-solid fa-star"></i>   
+                <i class="fa fa-solid fa-star"></i>  
+                <i class="fa fa-solid fa-star"></i>              
+            </div>
+            <div class="pricing mb-2">
+                <div class="pricing-price">
+                    <h1>Potensi Jual</h1>
+                    <p>Rp. 60.000</p>
+                </div>
+                <div class="pricing-margin">
+                    <h1>Margin</h1>
+                    <p>50%</p>
+                </div>
+            </div>
+            <div class="pricing-detail">
+                <ul class="title">
+                    <li><i class="fa fa-coins mr-2"></i></i>Margin</li>
+                    <li><i class="fa fa-coins mr-2"></i>Minimum Jual</li>
+                    <li><i class="fa fa-coins mr-2"></i>HPP Jual</li>
+                </ul>
+                <ul class="value">
+                    <li>80.000</li>
+                    <li>69.000</li>
+                    <li>14.500</li>
+                </ul>
+            </div>
+        </div>
+        <div class="mb-3 d-flex justify-content-around mt-3">
+            <a href="#" class="btn btn-product">
+                <span>Lihat</span>    
+            </a>
+                
+        </div>
+    </div>
+</div>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,89 +6,12 @@
             </div>                 
         </x-slot>
         
-        <div class="row">
-            <div class="col-xl-3 col-md-6 mb-4">
-                <div class="card border-left-warning shadow h-100 py-2 bg-success">
-                    <div class="card-body">
-                        <div div class="row no-gutters align-items-center ">
-                            <div class="col mr-2">
-                                <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                Jumlah Barang
-                                </div>
-                                <div class="h5 mb-0 font-weight-bold text-gray-800">
-                                    23 pcs
-                                </div>
-                            </div>
-                            <div class="col-auto">
-                                <i class="fas fa-calendar fa-2x text-gray-300"></i>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-xl-3 col-md-6 mb-4">
-                <div class="card border-left-primary shadow h-100 py-2 bg-warning">
-                    <div class="card-body">
-                        <div div class="row no-gutters align-items-center">
-                            <div class="col mr-2">
-                                <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                Sedang Diproses
-                                </div>
-                                <div class="h5 mb-0 font-weight-bold text-gray-800">
-                                    23 pcs
-                                </div>
-                            </div>
-                            <div class="col-auto">
-                                <i class="fas fa-calendar fa-2x text-gray-300"></i>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-xl-3 col-md-6 mb-4">
-                <div class="card border-left-primary shadow h-100 py-2 bg-danger">
-                    <div class="card-body">
-                        <div div class="row no-gutters align-items-center">
-                            <div class="col mr-2">
-                                <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                Selesai
-                                </div>
-                                <div class="h5 mb-0 font-weight-bold text-gray-800">
-                                    23 pcs
-                                </div>
-                            </div>
-                            <div class="col-auto">
-                                <i class="fas fa-calendar fa-2x text-gray-300"></i>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-xl-3 col-md-6 mb-4">
-                <div class="card border-left-primary shadow h-100 py-2 bg-green">
-                    <div class="card-body">
-                        <div div class="row no-gutters align-items-center">
-                            <div class="col mr-2">
-                                <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                Saldo
-                                </div>
-                                <div class="h5 mb-0 font-weight-bold text-gray-800">
-                                    23 pcs
-                                </div>
-                            </div>
-                            <div class="col-auto">
-                                <i class="fas fa-calendar fa-2x text-gray-300"></i>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+        <div class="row row-cols-1 row-cols-md-3 row-cols-sm-6 g-2">
+            <x-product-card/>
+            <x-product-card/>
+            <x-product-card/>
+            <x-product-card/>
         </div>
-    </div>
-    
-    <x-float-whatsapp>
-    </x-float-whatsapp>
+
+        <x-float-whatsapp/>
 </x-app-layout>
