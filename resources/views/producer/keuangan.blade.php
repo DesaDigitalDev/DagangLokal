14ba7a054b62967ec51874e5babd2b66857f90d7
habidinadeprayoga <habidin.ade@gmail.com>
2023-08-27 21:17:42 +0800

    tambah 'Rp'

--- resources/views/producer/keuangan.blade.php
+++ resources/views/producer/keuangan.blade.php
@@ -31,7 +31,7 @@
                                     @if (is_null($dtSaldo))
                                         0
                                     @elseif (!is_null($dtSaldo))
-                                        {{ $dtSaldo->balance }}
+                                        Rp.{{ $dtSaldo->balance }}
                                     @endif
                                 </div>
                             </div>
@@ -65,7 +65,7 @@
                             <div class="col mr-2">
                                 <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Total Withdraw
                                 </div>
-                                <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $dtTrxUserTotal }}</div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">Rp.{{ $dtTrxUserTotal }}</div>
                             </div>
                             <div class="col-auto">
                                 <i class="fas fa-users fa-2x text-gray-300"></i>
